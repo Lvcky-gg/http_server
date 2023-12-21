9fcec84ba34ea1da6ba82423b251152048bb659f
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-21 16:09:11 -0600

    more work

--- src/http/request.rs
+++ src/http/request.rs
@@ -21,5 +21,3 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
-
-}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 pub struct Server {
         addr: String,
     }
@@ -19,18 +22,25 @@ impl Server{
                         let mut buffer = [0; 1024];
                         match stream.read(&mut buffer){
                             Ok(_)=>{
-                                println!("Recieved a request: {}",String::from_utf8_lossy(&buffer))
+                                println!("Recieved a request: {}",String::from_utf8_lossy(&buffer));
+
+                               match Request::try_from(&buffer[..]){
+                                Ok(request)=>{},
+                                Err(e)=> println!("Failed to parse a request:{}",e)
+
                             }
+
+                        }
                             Err(e)=>println!("Failed to read connection: {}",e)
                         }
-                        
+
                     },
                     Err(e)=>println!("Failed: {}",e)
 
                 }
 
          }
-       
+
     }
 
 }
