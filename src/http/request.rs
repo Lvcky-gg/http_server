a896cfe4ddfba7628d318804fc088fc83ee9889a
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-21 18:10:51 -0600

    n/a

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,7 @@
-
+use std::error::Error;
 use super::method::Method;
 use std::convert::TryFrom;
+use std::fmt::Display;
 pub struct Request {
     path: String,
     query_string: Option<String>,
@@ -21,4 +22,20 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
+
+
 }
+
+impl Display for ParseError {
+
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+
+}
+
+impl Error for ParseError{}
