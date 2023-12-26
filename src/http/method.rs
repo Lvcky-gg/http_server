826f9545c81024992d18bfbc632c493f83cc63ea
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-26 09:11:33 -0600

    debug

--- src/http/method.rs
+++ src/http/method.rs
@@ -19,14 +19,14 @@ impl FromStr for Method {
     fn from_str(s: &str) -> Result<Self, Self::Err>{
         match s {
             "GET"=> Ok(Self::GET),
-            "POST"=> Ok(Self.POST),
-            "PUT" => Ok(Self.PUT),
-            "DELETE" => Ok(Self.DELETE),
-            "HEAD" => Ok(Self.HEAD),
-            "CONNECT" => Ok(Self.CONNECT),
-            "OPTIONS" => Ok(Self.OPTIONS),
-            "TRACE" => Ok(Self.TRACE),
-            "PATCH" => Ok(Self.PATCH),
+            "POST"=> Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "DELETE" => Ok(Self::DELETE),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
             _=> Err(MethodError),
         }
 
