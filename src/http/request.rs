95783ec61b3f3a1390acfa19b9119a47cc64430e
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-26 09:21:00 -0600

    debug

--- src/http/request.rs
+++ src/http/request.rs
@@ -26,7 +26,7 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1"{
@@ -34,6 +34,14 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[1 +1..]);
+            path = &path[..i];
+        }
 
         unimplemented!()
     }
